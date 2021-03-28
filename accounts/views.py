b907999da65000443f7ac7af3d534d81a23d992f
최준원 <cjw1102@naver.com>
2021-03-28 23:29:13 +0900

    [Feat] Signup Custom

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods, require_POST, require_safe
 from django.contrib.auth import login as auth_login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 @require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('community:index')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     context = {
         'form': form,
     }
