d12cf4d65c3a3005a6cf8b7d1ab92fb65f4b5127
최준원 <cjw1102@naver.com>
2021-03-28 23:37:41 +0900

    [Feat] Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm
 
 # Create your views here.
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('community:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'community:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
 @require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.method == 'POST':
