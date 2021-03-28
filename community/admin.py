b2d06081fd770c891bc33f2d0686fbad79e9aa15
최준원 <cjw1102@naver.com>
2021-03-28 22:11:40 +0900

    [Set] Admin

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review, Comment
 
 # Register your models here.
+class ReviewAdmin(models.ModelAdmin):
+    list_display = ('pk', 'title', 'movie_title', 'rank', 'content', 'created_at', 'updated_at',)
+
+class CommentAdmin(models.ModelAdmin):
+    list_display = ('pk', 'content', 'created_at', 'updated_at',)
+
+admin.site.register(Review, ReviewAdmin, Comment, CommentAdmin)
