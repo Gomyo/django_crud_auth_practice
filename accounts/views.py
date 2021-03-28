5d78adfa69ff0c3009a1fb8e33da2783ca8b20f0
최준원 <cjw1102@naver.com>
2021-03-29 00:10:42 +0900

    [Feat] Auth delete

--- accounts/views.py
+++ accounts/views.py
@@ -44,3 +44,10 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/signup.html', context)
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect('community:index')
