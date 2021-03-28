70e4d8a5cc067696dbcdccbd1636d6c4da1765d0
최준원 <cjw1102@naver.com>
2021-03-28 23:28:15 +0900

    [Feat] Signup auth.User

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('community:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
